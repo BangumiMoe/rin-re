e6a235ce34a1a6858b1d3224a3c5237f59a6c350
IntPtr <lxy.lixiaoyan@gmail.com>
2017-11-23 21:31:53 +0800

    add web font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
+import "./fonts";
 import "./index.css";
 
 import App from "./App";
