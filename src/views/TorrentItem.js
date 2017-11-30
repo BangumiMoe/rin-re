f7e782033218dc9034cc7bc01e94e3087d4bbc6d
IntPtr <lxy.lixiaoyan@gmail.com>
2017-11-30 14:57:35 +0800

    add category

--- src/views/TorrentItem.js
+++ src/views/TorrentItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
+import Translate from "./Translate";
 import ButtonBase from "./ButtonBase";
 import TorrentMeta from "./TorrentMeta";
 
@@ -21,9 +22,17 @@ class TorrentItem extends React.Component {
           className="TorrentItem"
           to={`/torrent/${item.id}`}
         >
-          <h1 className="TorrentItem-title">{item.title}</h1>
-          <div className="TorrentItem-meta">
-            <TorrentMeta item={item} />
+          <div className="TorrentItem-category">
+            <Translate
+              value={item.categoryTag.name}
+              locales={item.categoryTag.locale}
+            />
+          </div>
+          <div className="TorrentItem-content">
+            <h1 className="TorrentItem-title">{item.title}</h1>
+            <div className="TorrentItem-meta">
+              <TorrentMeta item={item} />
+            </div>
           </div>
         </ButtonBase>
       </article>
